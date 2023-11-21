c9810ea588f33ce9f7432d187380fde685705e34
George Cook <georgehhcook@gmail.com>
2023-11-21 14:43:17 +0000

    add task done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,25 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    raise
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def task
+    @task = Task.find(params[:id])
+  end
 end
